b176f8f5a629d8adc2038eaecef5dd825cd63671
Tim Johnson <29287358+atari2600tim@users.noreply.github.com>
2023-05-10 11:17:17 -0400

    snippet from SlySven on accessibility channel

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -4638,7 +4638,8 @@ bool mudlet::desktopInDarkMode()
 
 void mudlet::announce(const QString& text, const QString& processing)
 {
-    mpAnnouncer->announce(text, processing);
+    QTextEdit convertor(text);
+    mpAnnouncer->announce(convertor.toPlainText(), processing);
 }
 
 void mudlet::onlyShowProfiles(const QStringList& predefinedProfiles)
